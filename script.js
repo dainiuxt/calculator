141c8286ac0ecfb86203392152d2c98bcd559b58
Dainius <dainiuxt@gmail.com>
2021-02-03 18:40:22 +0000

    Add NumPad keycodes

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let result = 0;
 const display = document.querySelector('.input');
 const clear = document.querySelector('.is-clear');
+
+function getKey(e) {
+  const key = document.querySelector(`.calc-button[data-key="${e.keyCode}"]`);
+  console.log(key);
+}
+
 display.textContent = result;
 result = '563.134e+15';
 display.textContent = result;
@@ -9,3 +15,5 @@ clear.addEventListener('click', function(e) {
   result = 0;
   display.textContent = result;
 });
+
+window.addEventListener('keydown', getKey);
