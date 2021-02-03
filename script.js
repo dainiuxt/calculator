dae62e519ff1626a9c3f231689677723130d36d1
Dainius <dainiuxt@gmail.com>
2021-02-03 19:36:45 +0000

    Add some key functionality

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 let result = 0;
 const display = document.querySelector('.input');
-const clear = document.querySelector('.is-clear');
+// const clear = document.querySelector('.is-clear');
 
 function getKey(e) {
   const key = document.querySelector(`.calc-button[data-key="${e.keyCode}"]`);
-  console.log(key);
+  const keyClass = key.classList;
+  if (keyClass[1] === 'is-clear') {
+    result = 0;
+    display.textContent = result;
+  }
+  console.log(keyClass);
+}
+
+function clickMouse(e) {
+  const key = document.querySelector(`.calc-button[data-key="${e.keyCode}"]`);
+  // Get key classList on mouse click.
+  const keyClass = key.classList;
+  if (keyClass[1] === 'is-clear') {
+    result = 0;
+    display.textContent = result;
+  }
 }
 
 display.textContent = result;
 result = '563.134e+15';
 display.textContent = result;
 
-clear.addEventListener('click', function(e) {
-  result = 0;
-  display.textContent = result;
-});
-
+window.addEventListener('click', clickMouse);
 window.addEventListener('keydown', getKey);
